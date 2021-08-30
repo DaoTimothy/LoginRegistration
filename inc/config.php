e9c38611b0a6efc0bb87cdbfccb133ce27e5c59a
DaoTimothy <timothydao273@gmail.com>
2021-08-30 15:36:10 -0400

    Set up database

--- inc/config.php
+++ inc/config.php
@@ -3,4 +3,9 @@
     if(!defined('__CONFIG__')) {
         exit('You do not have a config file');
     }
+
+    //include DB.php
+    include_once "classes/DB.php";
+
+    $con = DB::getConnection();
 ?>
